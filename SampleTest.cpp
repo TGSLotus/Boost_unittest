1da86679dfbfe43dd611739e9357a47f478b0173
AWSYS-I\user1.gaio <reynaldminguez@gmail.com>
2023-03-27 14:14:54 +0800

    Updated Test File

--- SampleTest.cpp
+++ SampleTest.cpp
@@ -15,24 +15,3 @@ BOOST_AUTO_TEST_CASE( another_test_case ){
 	BOOST_TEST ( a == b);
 }
 
-BOOST_AUTO_TEST_CASE(another_wan) {
-// union
-
-    union {int i; float f;} u;
-    u.i = 34;
-    u.f = 2.3;
-
-    boost::variant<int, string> u1, u2;
-    u1 = 2;
-    u2 = "hello";
-    cout << u1 << "" << u2 << endl;
-
-    u1 = boost::get<int>(u1)*2;
-    // string st = boost::get<string>(u1);
-    u1 = "good";
-    u2 = 32;
-
-    boost::variant<int, string> u3;
-    cout << u3 << endl;
-
-}
