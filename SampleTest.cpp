66bba3b2f13c6df5d0fe8cb185779fe6febf8710
AWSYS-I\user1.gaio <reynaldminguez@gmail.com>
2023-03-28 14:05:24 +0800

    Updated

--- SampleTest.cpp
+++ SampleTest.cpp
@@ -4,12 +4,12 @@
 
 using namespace std;
 
-BOOST_AUTO_TEST_CASE( your_test_case ) {
+BOOST_AUTO_TEST_CASE( pass_test_case ) {
 	    std::vector<int> a{1, 2};
 	        std::vector<int> b{1, 2};
 		    BOOST_TEST( a == b );
 }
-BOOST_AUTO_TEST_CASE( another_test_case ){
+BOOST_AUTO_TEST_CASE( fail_test_case ){
 	std::string a = "a";
 	std::string b = "b";
 	BOOST_TEST ( a == b);
